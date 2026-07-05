b6527b851037274c87c20a6cde2a3361410dcdec
Jeiker-G <golovco697@gmail.com>
2026-07-05 22:20:37 +0200

    Update Google Sheets credentials and fix Streamlit interface

--- app.py
+++ app.py
@@ -2,21 +2,19 @@ import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 
-# Настройка подключения к Google Sheets
 @st.cache_resource(ttl=600)
 def get_sheet():
-    # Данные сервисного аккаунта (вставлены напрямую в код для стабильности)
     creds_dict = {
         "type": "service_account",
-        "project_id": "alien-airfoil-404006",
-        "private_key_id": "20520e7376090739b8f6f7347be15ae1c9424b63",
-        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDIRCt4MNkXGufS\nHLRqpSUMchwGcGxo9TOaYx1KP71KRq1M/nwWl7Jjq5GM6dHSvi1saxHU0HWLX6/4\nZoXErxcm8wlN29W2P2cJSqEVcd0WhgRkTP0Jexul9a9kNfo6X0Zn7C1cmk+9oJX6\nGoGKjew9+0c6WyIIdCXDj3ykfu2rlLR9BaM4t+pM56sKFEGhdta1eF/MB6NvAa9e\nKJ82Bpb14kWsp882EKTTO/VU0zvNcxktvMJYsR1DHzCL9cKmBJsZ1qhdwW+10eZJ\nnvJhLzuDEW2a4Y8Cyqrw9/hcBC9UsrD7m57jN3KxU9+NuGgUYdL2pAOVEbqk9ei4\n41oI8bL3AgMBAAECggEAA63I59c4R0xHVOmremGlQBWUJGqKFOJOokS1PYkIagxi\n7TN0xeXfsSTUmPRhb9y7F3ve1hXfcU6x/CV5/WZ2Z1Dda2PUVbNtdtMhowC2XhsN\n8P+/Dp6hBksjzGEqqX0/KfQoBl4pUXAmDfQEGPtjCxjW5dOijB0CGPxFfsoVuJ1y\nEUg+RDzubYmlQkbMgDlrC7li4bcYLVUSjImy4sQP1OXlhdgo3OTuxebDBWc0o3R7\nhXuFqZTKKDti4qd0Xu/zSXR52YaPs0Wvk8x2GyXLKD52cR0xDr1WlU5uqja1HVNF\nuO1Aj0SspserxnCY+u2qvHrujwmj8hz23I7u/AGBfQKBgQDyMmCGqGO7CYC9SkKD\n0yW5hlLKK/uPMLddplc/UgGcWVsmel2NSvM6bWAF3YiM8gRNQlaBiEisxwrMFm6W\nUtWMSNZMjKhhEqj9BeTnUH9LV+5aTbY216uLNIuflpoJzx09+N4Tz1PybQbcSo7C\n6yp8DXl9xeOwclKUC6h+31yWZQKBgQDTrggbgSr7V7cNk79fumNi5os/Wx0Xk3xL\nS0Zt2Pkio6U00D6U9wuKEwcN8fAnAiaGQ+803OZ22/5LkkTNXFKrmK4r4wrjwvPp\nAz7Q59a5G4Dq3KHFWoU63BUVoVXKaz19g/J+fCPSNSJ4+pKJjfS/WcRW/fCnTsoq\nJLbx7ZOwKwKBgCsQhBNNe7Y1IVwHCT6xW6LaXHwwR+GVEvWm7xqGNV0cklWMKnhG\nHGuGKS3Bz1bobZVjN0h++BUoqyXQ00cNYYU5KkmgNQxVqhCbbLEoogm+j9YkvUKR\nJKJy7GSq/abC2fs79hjlo5KWikK/SxUjhYFbT//qIOst2LsUO5iblLX1AoGANe1I\nM3mcsZuPDu5s+r5JQ5DoOKPFrZPtPmFW9/UmwyRdmi7TQrANSIfAbFUOkWGC3wG/\nlwAP1ogFKMuCq66xgPXMqXGV/KBV8y7YefAWS+It3aeHrvd4qZ4X4QLREFhxueA7\nju2hUpQmzBtJ6bE4gOcllePrwzPXXRUtZkTYMzECgYEAjGX8zFdTSRTrYq5OrcTI\nHl9AG4G4YqX10lnp4rSmT6EjmgoO/zegLe+4p3VtZmVEPBQ7UNosiTZibt9Ulfhu\njSwq7K0WK7p5irLira/XvZ/t5fsrbDHj5agtsctsSz9lw7hTxYbLM8GAiEH8LfGG\neRg+ZLOywdD4UPL8niSRa/c=\n-----END PRIVATE KEY-----",
-        "client_email": "my-grafik-app@alien-airfoil-404006.iam.gserviceaccount.com",
-        "client_id": "114155465166172575794",
+        "project_id": "grafik-bot-501520",
+        "private_key_id": "46e830bb8517ee0e37273590309cdf67a105ad9a",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEuwIBADANBgkqhkiG9w0BAQEFAASCBKUwggShAgEAAoIBAQC7wubfBjLIFb7k\nsn+VO6+T+RTfhRLEbKejBAG/rjAbebJRMdafOe0O/PZmrrk2HXJFCUBD4iY3OBLT\nHQ9MS9EM2RHAQo+lLyg5uAFI/ZmRIu1hlWJ7F74Vdk8nNMUCTvt6f38EdfNRUdCb\nsbtnXrzqkSc6yqbJPQxJGlnO11dRvDOJKBiUg1adQwYNtRXa11a1+J3IZpmx4kO6\nhq78IRRL07ZHirT/PJbnDqkDXlZZFWRV5q59XkdHe4IvhAYWO3jo2rb+Z5BH0+MJ\nth/vUvVL44Jh4ugnuwTcPKKw0al4wvNDTO4B8//gJPndkIHiMj3zlgh95hbMmPDD\nBrsym5XJAgMBAAECgf8FhzUhEbi3NPipRFBeOYsggnHsOsHJIQ7tlk6FOrv9oEK2\ng2A9kIHP+ezeBfMyPwHVUK/Qtrn0Fhqi2Z3d4JvJebAxMStiEC2jzS8wihvf3rW+\n0nO1VMMCLbaSjth+r1XU3WiIpMCLNSQKu6dU19iF2Hh0TXJpcNRYoZyjmrOBBw5y\nMdS46igLX+GkLPyIcxmjjwXrFMmmlnD4wIJblUJtNfuU263A2VCzZyYgHOOEiIBf\nYomlYzbC/OZay/FSCEJfOlArDNW577iwNGaX+aKrHlVQbZeqg1aOeekxzi8TLkBe\nc4VKnZxMz+aqQbp2lXleho9pT4FROjnWCwmV7o8CgYEA+R0iKrmvOvi3aAawvpbP\nC/6Aogf/9jLCIqKdKckCCkxY0fpyICeo3VU1oFw8ImctSXb9GV+nBVPtMd+tZqGi\nIqw4Hg6TLF+9FB3p3DgF9CF5Md8LxXE7I9qb/i7VB/7ouxmDhjCw/uxwTKYktXHJ\nT5q3TMAb1vukG3gf2w2aRFMCgYEAwPOawThg4D7fFAu9+lBcfQmGAIBjnEEEj+u7\nqrzDm68RklE7OwlElKYNwEUMIvkAXyQpGTkDl6pcY8jEv1jiBGuTvSpGjwzmv2q8\nYl8le2qjtdVhh0+5CBD68WVfWdXm8PZ2qENYvftpSLv7PefJz2dScvhMsCAwT2eJ\nkDlo+fMCgYBI/TI/lnwzKSCDyEIM9Z7ggscpsnJarnCXOVIXJiAE6G21yE2cAkLF\n5ut5eszbv6NTPRvXTWFgfECpNkJWKdGZmyAuHlMc00hRFpqovXlxGEWsSQ/n389W\nJ/1AsnQUjame+FDDBo01hA07SArFZvt26Xv/buI5US26Jq4evpOOpQKBgQCcq7C2\nv2555tL/1eD2RRY0OO1jrtwy3eA0dDa3lfpGiUp9QhQUBZx4DrUPr4EBTKnultAA\nOPkfxyappzWKIUO0Tx1w9cjBErqyJpq8TYGDyr0PQimk0yjs0czRAX0A1txkP9tt\nGPIPoPqGIo0IYRLOo5ig3dh3Ekvnzaw3PUZt3QKBgEUjOGSjcLPDu0dCqwOzlrFx\nx+OTpK5F2eVraE9gVPFo9sIVLKzDHt7WMqSPGnoRkGqwEGo9pQmpaF/V6ahssVCQ\nwUq/JhCIoRQXfi/VtpaJnTOIgehuXgeQDJDO8byVr60UxZ+zKefQM4oS0cOMnO41\nqYfpQyLQTZqfSmKiYW/M\n-----END PRIVATE KEY-----",
+        "client_email": "my-grafik-app@grafik-bot-501520.iam.gserviceaccount.com",
+        "client_id": "112388178246657528769",
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/my-grafik-app%40alien-airfoil-404006.iam.gserviceaccount.com"
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/my-grafik-app%40grafik-bot-501520.iam.gserviceaccount.com"
     }
     
     creds = Credentials.from_service_account_info(creds_dict)
@@ -25,7 +23,6 @@ def get_sheet():
         "https://www.googleapis.com/auth/drive"
     ])
     client = gspread.authorize(scoped_creds)
-    # Открываем таблицу по ID из Secrets
     return client.open_by_key(st.secrets["SHEET_ID"]).get_worksheet(0)
 
 def load_data():
@@ -41,9 +38,7 @@ def save_data(db):
     if rows:
         sheet.append_rows(rows)
 
-# Интерфейс Streamlit
 st.title("График дежурства")
-
 db = load_data()
 name_input = st.text_input("Введите имя:", key="name_in")
 
@@ -52,7 +47,7 @@ if st.button("Записаться на 10:00"):
         db['cell_10_00'] = name_input.strip()
         save_data(db)
         st.success("Сохранено!")
-        st.rerun() # Обновляем страницу для отображения данных
+        st.rerun()
     else:
         st.warning("Введите имя!")
 
